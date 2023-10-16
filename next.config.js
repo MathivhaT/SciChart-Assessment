3a998cc9dd3da1fdc2c16b21560ed3d47a69856c
Talifhani Mathivha <talifhanim@msbe.co.za>
2023-10-16 15:18:28 +0200

    SciChart Assessment Complete

--- next.config.js
+++ next.config.js
@@ -1,4 +1,20 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const CopyPlugin = require("copy-webpack-plugin");
 
-module.exports = nextConfig
+module.exports = {
+    basePath: '',
+    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+        // Note: we provide webpack above so you should not `require` it
+        // Perform customizations to webpack config
+        const destWasmFolder = "static/chunks/app";
+        config.plugins.push(new CopyPlugin({
+            patterns: [
+                { from: "node_modules/scichart/_wasm/scichart2d.data", to: destWasmFolder },
+                { from: "node_modules/scichart/_wasm/scichart2d.wasm", to: destWasmFolder },
+            ]
+        }),)
+
+        // Important: return the modified config
+        return config
+    },
+}
+
